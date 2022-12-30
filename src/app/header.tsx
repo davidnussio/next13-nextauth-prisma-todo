c406d7ebd858b240b6610b43be4fd5ae8bfcb090
David Nussio <david.nussio@gmail.com>
2022-12-30 20:36:30 +0100

    fix: 404

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,5 +1,6 @@
 import type { User } from "next-auth";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { getCurrentUser } from "~/server/common/get-server-auth-session";
 import ProtectedHeader from "./protected-header";
 import PublicHeader from "./public-header";
@@ -7,12 +8,17 @@ import PublicHeader from "./public-header";
 // next page
 export default async function Header() {
   const user = (await getCurrentUser()) as User;
+  const router = useRouter();
 
   return (
     <header className="flex items-center justify-between bg-white/10 p-3">
-      <Link href="/" prefetch={false} className="text-xl font-bold">
-        Home
-      </Link>
+      {router.pathname !== "/" ? (
+        <Link href="/" prefetch={false} className="text-xl font-bold">
+          Home
+        </Link>
+      ) : (
+        "Home"
+      )}
       {user ? <ProtectedHeader /> : <PublicHeader />}
     </header>
   );
--- src/app/protected-header.tsx
+++ src/app/protected-header.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 // next page
 export default function ProtectedHeader() {
+  const router = useRouter();
+
   return (
     <>
-      <Link href="/todo" className="text-xl font-bold">
-        Todo
-      </Link>
+      {router.pathname !== "/todo" ? (
+        <Link href="/todo" className="text-xl font-bold">
+          Todo
+        </Link>
+      ) : (
+        "Todo"
+      )}
       <div>{/* add an avatar image here */}avatar</div>
     </>
   );
